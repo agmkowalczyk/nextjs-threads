82ea16bf1e278b8093f6935b14a4b4a7237fef10
agmkowalczyk <agm.kowalczyk@gmail.com>
2024-01-26 21:47:49 +0100

    Add layout structure

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,6 +2,12 @@ import { ClerkProvider } from '@clerk/nextjs'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import '../globals.css'
+import {
+  Bottombar,
+  LeftSidebar,
+  RightSidebar,
+  Topbar,
+} from '@/components/shared'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,7 +24,17 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang='en'>
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <Topbar />
+          <main>
+            <LeftSidebar />
+            <section className='main-container'>
+              <div className='w-full max-w-4xl'>{children}</div>
+            </section>
+            <RightSidebar />
+          </main>
+          <Bottombar />
+        </body>
       </html>
     </ClerkProvider>
   )
---
+++ components/shared/index.ts
@@ -0,0 +1,11 @@
+import Bottombar from './Bottombar'
+import LeftSidebar from './LeftSidebar'
+import RightSidebar from './RightSidebar'
+import Topbar from './Topbar'
+
+export {
+  Bottombar,
+  LeftSidebar,
+  RightSidebar,
+  Topbar,
+}
