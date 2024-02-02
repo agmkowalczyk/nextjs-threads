e8175d61b59a1723ac6d504ac28bb3c23cf22079
agmkowalczyk <agm.kowalczyk@gmail.com>
2024-02-02 11:32:35 +0100

    Thread actions

---
+++ lib/actions/thread.actions.ts
@@ -0,0 +1,74 @@
+'use server'
+
+import { revalidatePath } from 'next/cache'
+import Thread from '../models/thread.model'
+import User from '../models/user.model'
+import { connectToDB } from '../mongoose'
+
+interface Params {
+  text: string
+  author: string
+  communityId: string | null
+  path: string
+}
+
+export async function createThread({
+  text,
+  author,
+  communityId,
+  path,
+}: Params) {
+  connectToDB()
+
+  try {
+    const createdThread = await Thread.create({
+      text,
+      author,
+      community: null,
+    })
+
+    await User.findByIdAndUpdate(author, {
+      $push: { threads: createdThread._id },
+    })
+
+    revalidatePath(path)
+  } catch (error: any) {
+    throw new Error(`Failed to create thread: ${error.message}`)
+  }
+}
+
+export async function fetchPosts(pageNumber = 1, pageSize = 20) {
+  connectToDB()
+
+  const skipAmount = (pageNumber - 1) * pageSize
+
+  try {
+    const postsQuery = Thread.find({
+      parentId: { $in: [null, undefined] },
+    })
+      .sort({ createdAt: 'desc' })
+      .skip(skipAmount)
+      .limit(pageSize)
+      .populate({ path: 'author', model: User })
+      .populate({
+        path: 'children',
+        populate: {
+          path: 'author',
+          model: User,
+          select: '_id name parentId image',
+        },
+      })
+
+    const totalPostCount = await Thread.countDocuments({
+      parentId: { $in: [null, undefined] },
+    })
+
+    const posts = await postsQuery.exec()
+
+    const isNext = totalPostCount > skipAmount + posts.length
+
+    return { posts, isNext }
+  } catch (error: any) {
+    throw new Error(`Failed to fetch threads: ${error.message}`)
+  }
+}
