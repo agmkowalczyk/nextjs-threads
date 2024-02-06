af5f5c7cacad277f925da5de069cffdb0cccc5d6
agmkowalczyk <agm.kowalczyk@gmail.com>
2024-02-06 18:48:58 +0100

    fetchUserPosts

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -142,3 +142,28 @@ export async function addCommentToThread(
     throw new Error(`Failed to add comment to thread: ${error.message}`)
   }
 }
+
+export async function fetchUserPosts(userId: string) {
+  try {
+    connectToDB()
+
+    const threads = await User.findOne({ id: userId })
+      .populate({
+        path: 'threads',
+        model: Thread,
+        populate: {
+          path: 'children',
+          model: Thread,
+          populate: {
+            path: 'author',
+            model: User,
+            select: 'name image id'
+          }
+        }
+      })
+    
+    return threads
+  } catch (error: any) {
+    throw new Error(`Failed to fetch user threads: ${error.message}`)
+  }
+}
