0f8fc140e984dfd4acd0e143f427fcb1d928c45e
agmkowalczyk <agm.kowalczyk@gmail.com>
2024-02-10 12:01:57 +0100

    fetch community

--- lib/actions/community.actions.ts
+++ lib/actions/community.actions.ts
@@ -49,4 +49,111 @@ export async function createCommunity(
   }
 }
 
+export async function fetchCommunityDetails(id: string) {
+  try {
+    connectToDB()
+
+    const communityDetails = await Community.findOne({ id }).populate([
+      'createdBy',
+      {
+        path: 'members',
+        model: User,
+        select: 'name username image _id id',
+      },
+    ])
+
+    return communityDetails
+  } catch (error) {
+    // Handle any errors
+    console.error('Error fetching community details:', error)
+    throw error
+  }
+}
+
+export async function fetchCommunityPosts(id: string) {
+  try {
+    connectToDB()
+
+    const communityPosts = await Community.findById(id).populate({
+      path: 'threads',
+      model: Thread,
+      populate: [
+        {
+          path: 'author',
+          model: User,
+          select: 'name image id', // Select the "name" and "_id" fields from the "User" model
+        },
+        {
+          path: 'children',
+          model: Thread,
+          populate: {
+            path: 'author',
+            model: User,
+            select: 'image _id', // Select the "name" and "_id" fields from the "User" model
+          },
+        },
+      ],
+    })
+
+    return communityPosts
+  } catch (error) {
+    // Handle any errors
+    console.error('Error fetching community posts:', error)
+    throw error
+  }
+}
+
+export async function fetchCommunities({
+  searchString = '',
+  pageNumber = 1,
+  pageSize = 20,
+  sortBy = 'desc',
+}: {
+  searchString?: string
+  pageNumber?: number
+  pageSize?: number
+  sortBy?: SortOrder
+}) {
+  try {
+    connectToDB()
+
+    // Calculate the number of communities to skip based on the page number and page size.
+    const skipAmount = (pageNumber - 1) * pageSize
+
+    // Create a case-insensitive regular expression for the provided search string.
+    const regex = new RegExp(searchString, 'i')
+
+    // Create an initial query object to filter communities.
+    const query: FilterQuery<typeof Community> = {}
+
+    // If the search string is not empty, add the $or operator to match either username or name fields.
+    if (searchString.trim() !== '') {
+      query.$or = [{ username: { $regex: regex } }, { name: { $regex: regex } }]
+    }
+
+    // Define the sort options for the fetched communities based on createdAt field and provided sort order.
+    const sortOptions = { createdAt: sortBy }
+
+    // Create a query to fetch the communities based on the search and sort criteria.
+    const communitiesQuery = Community.find(query)
+      .sort(sortOptions)
+      .skip(skipAmount)
+      .limit(pageSize)
+      .populate('members')
+
+    // Count the total number of communities that match the search criteria (without pagination).
+    const totalCommunitiesCount = await Community.countDocuments(query)
+
+    const communities = await communitiesQuery.exec()
+
+    // Check if there are more communities beyond the current page.
+    const isNext = totalCommunitiesCount > skipAmount + communities.length
+
+    return { communities, isNext }
+  } catch (error) {
+    console.error('Error fetching communities:', error)
+    throw error
+  }
+}
+
 
