18014369fcaf7572f0513f1a9d96630019e3bb59
agmkowalczyk <agm.kowalczyk@gmail.com>
2024-02-13 01:34:27 +0100

    Searching users

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -1,9 +1,14 @@
 import UserCard from '@/components/cards/UserCard'
+import Searchbar from '@/components/shared/Searchbar'
 import { fetchUser, fetchUsers } from '@/lib/actions/user.actions'
 import { currentUser } from '@clerk/nextjs'
 import { redirect } from 'next/navigation'
 
-async function Page() {
+async function Page({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | undefined };
+}) {
   const user = await currentUser()
 
   if (!user) return null
@@ -13,7 +18,7 @@ async function Page() {
 
   const result = await fetchUsers({
     userId: user.id,
-    searchString: '',
+    searchString: searchParams.q,
     pageNumber: 1,
     pageSize: 25,
   })
@@ -21,6 +26,8 @@ async function Page() {
   return (
     <section>
       <h1 className='head-text mb-10'>Search</h1>
+
+      <Searchbar routeType='search' />
 
       <div className='mt-14 flex flex-col gap-9'>
         {result.users.length === 0 ? (
