334f9367de8202b7ebf6d5363295b7a632db2b0b
agmkowalczyk <agm.kowalczyk@gmail.com>
2024-02-17 14:36:59 +0100

    Add suggested users

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -1,12 +1,17 @@
 import { fetchCommunities } from '@/lib/actions/community.actions'
 import { currentUser } from '@clerk/nextjs'
 import UserCard from '../cards/UserCard'
+import { fetchUsers } from '@/lib/actions/user.actions'
 
 async function RightSidebar() {
   const user = await currentUser()
-  if (!user) return null
 
   const suggestedCommunities = await fetchCommunities({ pageSize: 4 })
+
+  const suggestedUsers = user && await fetchUsers({
+    userId: user.id,
+    pageSize: 4,
+  })
 
   return (
     <section className='custom-scrollbar rightsidebar'>
@@ -38,6 +43,25 @@ async function RightSidebar() {
       </div>
       <div className='flex flex-1 flex-col justify-start'>
         <h3 className='text-heading4-medium text-light-1'>Suggested Users</h3>
+
+        <div className='mt-7 flex w-[350px] flex-col gap-10'>
+          {suggestedUsers && suggestedUsers.users.length > 0 ? (
+            <>
+              {suggestedUsers?.users.map((person) => (
+                <UserCard
+                  key={person.id}
+                  id={person.id}
+                  name={person.name}
+                  username={person.username}
+                  imgUrl={person.image}
+                  personType='User'
+                />
+              ))}
+            </>
+          ) : (
+            <p className='!text-base-regular text-light-3'>No users yet</p>
+          )}
+        </div>
       </div>
     </section>
   )
